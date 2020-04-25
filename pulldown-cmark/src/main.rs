17e07f1f32f1a03b0a8211206e69fcd7ff217743
vratnam <vadhrivenkata.ratnam@nagra.com>
2020-04-25 15:09:55 +0530

    Failure crate added to the file.
    Failure crate added to the file.

--- pulldown-cmark/src/main.rs
+++ pulldown-cmark/src/main.rs
@@ -1,11 +1,46 @@
 #![feature(proc_macro_hygiene)]
 
+extern crate failure;
+
 use std::io::Write;
 use clap::{clap_app, crate_version};
 use pulldown_cmark::{Parser, Options, html};
 use std::fs;
+use std::fmt;
 use maud::html;
 use std::fs::File;
+use failure::{Fail};
+
+pub struct AppErrors {
+    code: String,
+    reason: String
+}
+
+impl fmt::Display for AppErrors {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.code, self.reason)
+    }
+}
+
+impl fmt::Debug for AppErrors {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Debug ({}, {})", self.code, self.reason)
+    }
+}
+
+impl Fail for AppErrors {
+
+}
+
+
+impl From<std::io::Error> for AppErrors {
+    fn from(error: std::io::Error) -> Self {
+        AppErrors {
+            code: "0".to_string(),
+            reason: error.to_string()
+        }
+    }
+}
 
 fn wrap_block_in_html(partial_html: &str, css: Option<&str>) -> String {
     let res = html! {
@@ -28,13 +63,18 @@ fn wrap_block_in_html(partial_html: &str, css: Option<&str>) -> String {
     res.into_string()
 }
 
-fn main() {
+pub fn read_file (input_filename: String) -> Result<String, AppErrors> {
+    let string = fs::read_to_string(input_filename)?;
+    Ok(string)
+}
+
+fn main() -> Result<(), AppErrors> {
     let args_handler = clap_app!(pull_down_cmark =>
         (version: crate_version!())
         (author: "vadhri")
-        (@arg input:  -i --input +takes_value +required "Filename to look for mark up")
-        (@arg events: -e --events "Sets a custom config file")
-        (@arg output: -o --output +takes_value "output file to write html")
+        (@arg input:  -i --input +takes_value +required "Filename to look for mark up.")
+        (@arg events: -e --events "Sets a custom config file for events.")
+        (@arg output: -o --output +takes_value "Output file to write html")
         (@arg css: -c --css +takes_value "path to css file")
     ).get_matches();
 
@@ -43,10 +83,12 @@ fn main() {
     let output_file_print = args_handler.is_present("output");
     let input_css = args_handler.is_present("css");
 
-    let infile = fs::read_to_string(input_filename).expect("File not found !");
-    let mut outcome = String::new();
+    let infile = read_file(input_filename.to_string())?;
 
+    let mut outcome = String::new();
+    let mut css: Option<&str> = None;
     let mut options = Options::empty();
+
     options.insert(Options::ENABLE_STRIKETHROUGH);
     options.insert(Options::ENABLE_TABLES);
     options.insert(Options::ENABLE_FOOTNOTES);
@@ -61,8 +103,6 @@ fn main() {
 
     html::push_html(&mut outcome, parser);
 
-    let mut css: Option<&str> = None;
-
     if input_css {
         css = Some(args_handler.value_of("css").unwrap());
     }
@@ -74,6 +114,8 @@ fn main() {
         let mut output_file_fd = File::create(output_filename).unwrap();
         let _ignore = output_file_fd.write_all(&outcome.into_bytes());
     } else {
-        println!("HTML -> {:?}", outcome);
+        println!("{:?}", outcome);
     }
+
+    Ok(())
 }
