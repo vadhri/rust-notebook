de30293eac933bf34ea4f5dcccd9a56a1555ab4a
Venkata Ratnam Vadhri <vadhrivenkata.ratnam@nagra.com>
2020-05-07 09:22:01 +0530

    Re-arrange to different folders

---
+++ database-adapters/postgresql/target/rls/debug/deps/postgresql-1b59435c01c38dcf.d
@@ -0,0 +1,5 @@
+/Users/venkat/rust-notebook/database-adapters/postgresql/target/rls/debug/deps/postgresql-1b59435c01c38dcf.rmeta: src/main.rs
+
+/Users/venkat/rust-notebook/database-adapters/postgresql/target/rls/debug/deps/postgresql-1b59435c01c38dcf.d: src/main.rs
+
+src/main.rs:
---
+++ database-adapters/postgresql/target/rls/debug/deps/postgresql-6af90ae66075a769.d
@@ -0,0 +1,5 @@
+/Users/venkat/rust-notebook/database-adapters/postgresql/target/rls/debug/deps/postgresql-6af90ae66075a769.rmeta: src/main.rs
+
+/Users/venkat/rust-notebook/database-adapters/postgresql/target/rls/debug/deps/postgresql-6af90ae66075a769.d: src/main.rs
+
+src/main.rs:
