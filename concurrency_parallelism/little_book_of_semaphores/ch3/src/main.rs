0e89c952c7ae69d406f6a1d9055b218dc9d7788c
Venkata Ratnam Vadhri <vadhrivenkata.ratnam@nagra.com>
2020-06-28 09:32:29 +0530

    Re-arrange ch3 main.rs
    Re-arrange ch3 main.rs

--- concurrency_parallelism/little_book_of_semaphores/ch3/src/main.rs
+++ concurrency_parallelism/little_book_of_semaphores/ch3/src/main.rs
@@ -1,6 +1,50 @@
 mod tests {
     use tokio::runtime::Runtime;
     use crossbeam_channel::unbounded;
+    use tokio::sync::Semaphore;
+    use std::sync::{Arc};
+
+    #[test]
+    fn s_3_5_multiplex_with_limit () {
+        async fn multiplex_with_limit () {
+            let multiplex_limit = Arc::new(Semaphore::new(10));
+            let critical_section = Arc::new(Semaphore::new(1));
+            let mut i: u32 = 0;
+            let mut j: u32 = 0;
+            let mut k: u32 = 0;
+
+            for worker in 0..30 {
+                let multiplex_limit_clone = multiplex_limit.clone();
+                let critical_section_clone = critical_section.clone();
+
+                let _new_client = tokio::spawn(async move {
+                    let p1 = multiplex_limit_clone.try_acquire();
+
+                    if p1.is_ok() {
+                        let _p2 = critical_section_clone.acquire().await;
+
+                        // do some secret stuff.
+                        i += 1;
+                        j += 1;
+                        k += 1;
+
+                        println!("Doing secret stuff .. {:?}", i);
+
+                    } else {
+                        println!("Thread limit over 10 reached., {:?}", i);
+                    }
+                });
+            }
+        }
+
+        let fut = async {
+            multiplex_with_limit().await;
+        };
+
+        let mut rt = Runtime::new().unwrap();
+
+        rt.block_on(fut);
+    }
 
     #[test]
     fn s_3_3_1_crossbeam_rendezvous_signal_pattern() {
